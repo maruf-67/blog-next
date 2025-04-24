b400e9cac6d1753cd7784bd94c8efc2cf7730143
Abdullah Al Maruf <almaruf@softograph.com>
2025-04-25 00:36:58 +0600

    showed post to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,38 @@
-import Image from "next/image";
-import Link from "next/link";
+import prisma from "./utils/prisma";
 
-export default function Home() {
+async function getPost() {
+  const data = await prisma.blogPost.findMany({
+    select: {
+      id: true,
+      title: true,
+      content: true,
+      imageUrl: true,
+      published: true,
+      author: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+      createdAt: true,
+       },
+  });
+  return data;
+}
+
+export default async function Home() {
+  const posts = await getPost();
   return (
-    <div>
-      <h1>Hello World from index page</h1>
-      <Link href="/dashboard">Dashboard</Link>
-    </div>
+      <div className="grid grid-cols-1 gap-4">
+        {posts.map((post) => (
+          <div key={post.id} className="border p-4 rounded-lg shadow-md">
+            <h2 className="text-2xl font-semibold">{post.title}</h2>
+            <p>{post.content}</p>
+            <p className="text-sm text-gray-500">By {post.author.name}</p>
+            <p className="text-sm text-gray-500">{new Date(post.createdAt).toLocaleDateString()}</p>
+          </div>
+        ))}
+      </div>
+    
   );
 }
